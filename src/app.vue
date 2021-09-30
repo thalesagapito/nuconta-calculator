63ef8b3b99e916f6f0e2e97ba78b85dc05b66091
Thales Agapito <thsousa101@gmail.com>
2021-09-29 23:19:47 -0300

    add initial calculator components

--- src/app.vue
+++ src/app.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
+import NucontaCalculatorInput from './components/nuconta-calculator-input.vue'
+import NucontaCalculatorOutput from './components/nuconta-calculator-output.vue'
 </script>
 
 <template>
+  <div class="nuconta-calculator-wrapper">
+    <nuconta-calculator-input />
+    <nuconta-calculator-output />
+  </div>
 </template>
 
 <style>
+.nuconta-calculator-wrapper {
+  @apply flex flex-col sm:flex-row p-6 space-y-6;
+}
 </style>
