6923a2415e27cd7bf1b6ad812994746615905d0a
Thales Agapito <thsousa101@gmail.com>
2021-10-02 12:01:07 -0300

    add first deposit amount input buttons and label

--- src/components/calculator-input-first-deposit-input.vue
+++ src/components/calculator-input-first-deposit-input.vue
@@ -1,4 +1,10 @@
-<script setup lang="ts">
+<script lang="ts" setup>
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { useCurrencyFormatter } from '../composables/formatters'
+
+const INCREMENT_OR_DECREMENT_VALUE = 100
+
 const props = withDefaults(
   defineProps<{
     firstDepositAmount: number
@@ -12,13 +18,72 @@ const emit = defineEmits<{
   (e: 'update:firstDepositAmount', value: number): void
 }>()
 
+const { t } = useI18n()
+const { formatCurrency } = useCurrencyFormatter()
+
+const formattedFirstDepositAmount = computed(() => formatCurrency({ amount: props.firstDepositAmount }))
+
+function incrementFirstDepositAmount() {
+  emit('update:firstDepositAmount', props.firstDepositAmount + INCREMENT_OR_DECREMENT_VALUE)
+}
+
+function decrementFirstDepositAmount() {
+  emit('update:firstDepositAmount', props.firstDepositAmount - INCREMENT_OR_DECREMENT_VALUE)
+}
 </script>
 
 <template>
-  <p>
-    first deposit input
-  </p>
+  <div class="first-deposit-input-wrapper">
+    <div class="first-deposit">
+      {{ t('firstDeposit') }}
+    </div>
+
+    <div class="amount-wrapper">
+      <div class="first-deposit-amount">
+        {{ formattedFirstDepositAmount }}
+      </div>
+
+      <button class="button" :aria-label="t('decrement')" @click="decrementFirstDepositAmount">
+        <span class="line" aria-hidden="true" />
+      </button>
+
+      <button class="button" :aria-label="t('increment')" @click="incrementFirstDepositAmount">
+        <span class="line" aria-hidden="true" />
+        <span class="line transform rotate-90" aria-hidden="true" />
+      </button>
+    </div>
+  </div>
 </template>
 
 <style lang="postcss" scoped>
+.first-deposit-input-wrapper {
+  @apply flex flex-col;
+
+  .first-deposit {
+    @apply text-lg text-gray-300;
+  }
+
+  .amount-wrapper {
+    @apply flex justify-between items-center pt-2;
+
+    .first-deposit-amount {
+      @apply text-lg mr-auto;
+    }
+
+    .button {
+      @apply w-14 h-14 ml-4 flex justify-center items-center relative border border-brand-purple rounded-full transition;
+
+      .line {
+        @apply w-5 h-px absolute bg-brand-purple;
+      }
+
+      &:hover, &:focus {
+        @apply bg-brand-purple bg-opacity-10;
+      }
+      &:active {
+        @apply bg-brand-purple bg-opacity-30;
+      }
+    }
+  }
+}
 </style>
