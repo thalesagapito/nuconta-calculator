a14765fb01ae1a629ec354ddb395f93893911fe7
Thales Agapito <thsousa101@gmail.com>
2021-10-02 11:53:53 -0300

    update currency formatter to use i18n

--- src/composables/formatters.ts
+++ src/composables/formatters.ts
@@ -1,9 +1,41 @@
-export function formatAmountInCents(amountInCents: number) {
-  const floatAmount = Math.floor(amountInCents / 100)
-  const formatter = new Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'BRL',
+import { useI18n } from 'vue-i18n'
+
+type FormatCurrencyArgs = {
+  amount: number
+  useGrouping?: boolean
+  useSpaceAfterSymbol?: boolean
+}
+
+export function useCurrencyFormatter() {
+  const { n } = useI18n({
+    numberFormats: {
+      en: {
+        currency: {
+          style: 'currency',
+          currency: 'BRL',
+          part: true,
+        },
+        currencyWithoutGrouping: {
+          style: 'currency',
+          currency: 'BRL',
+          useGrouping: false,
+        },
+      },
+    },
   })
 
-  return formatter.format(floatAmount)
+  const addSpaceAfterSymbol = (value: string) => value.replace(/^(\D+)/, '$1 ')
+
+  const formatCurrency = (args: FormatCurrencyArgs) => {
+    const { amount, useGrouping = false, useSpaceAfterSymbol = true } = args
+    const format = useGrouping ? 'currency' : 'currencyWithoutGrouping'
+    const formattedAmount = n(amount, format)
+
+    if (useSpaceAfterSymbol) return addSpaceAfterSymbol(formattedAmount)
+    return formattedAmount
+  }
+
+  return {
+    formatCurrency,
+  }
 }
