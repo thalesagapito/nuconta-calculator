49dd81f168aedacd51c8ee784f68d807025f6170
Thales Agapito <thsousa101@gmail.com>
2021-10-02 14:04:16 -0300

    add month and deposit refs to app.vue

--- src/app.vue
+++ src/app.vue
@@ -1,17 +1,34 @@
 <script lang="ts" setup>
+import { ref } from 'vue'
 import CalculatorInput from './components/calculator-input.vue'
 import CalculatorOutput from './components/calculator-output.vue'
+
+const firstDepositAmount = ref(1000)
+const months = ref(1)
 </script>
 
 <template>
   <div class="calculator-wrapper">
-    <calculator-input />
-    <calculator-output />
+    <calculator-input
+      v-model:firstDepositAmount="firstDepositAmount"
+      v-model:months="months"
+      class="input"
+    />
+
+    <calculator-output
+      :months="months"
+      class="output"
+    />
   </div>
 </template>
 
 <style lang="postcss" scoped>
 .calculator-wrapper {
-  @apply flex flex-col sm:flex-row p-8;
+  @apply p-8 flex flex-col sm:flex-row;
+  .input {
+    @apply mb-20;
+  }
+  .output {
+  }
 }
 </style>
