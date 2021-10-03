6e82be12a540b32c8925a1bd0cfce1332e59fb4d
Thales Agapito <thsousa101@gmail.com>
2021-10-03 11:49:29 -0300

    improve get-tax-percentage tests

--- src/domain/nu-conta/get-tax-percentage.spec.ts
+++ src/domain/nu-conta/get-tax-percentage.spec.ts
@@ -1,16 +1,14 @@
 import { getTaxPercentage } from './get-tax-percentage'
 
 describe('get-tax-percentage', () => {
-  it('should decrease tax percentage when increasing period ', () => {
-    const taxPercentageFor1Month = getTaxPercentage({ period: 1 })
+  it('should decrease tax percentage when increasing period over breakpoints', () => {
     const taxPercentageFor6Months = getTaxPercentage({ period: 6 })
     const taxPercentageFor12Months = getTaxPercentage({ period: 12 })
     const taxPercentageFor18Months = getTaxPercentage({ period: 18 })
     const taxPercentageFor24Months = getTaxPercentage({ period: 24 })
 
-    expect(taxPercentageFor1Month).toBeGreaterThanOrEqual(taxPercentageFor6Months)
-    expect(taxPercentageFor6Months).toBeGreaterThanOrEqual(taxPercentageFor12Months)
-    expect(taxPercentageFor12Months).toBeGreaterThanOrEqual(taxPercentageFor18Months)
-    expect(taxPercentageFor18Months).toBeGreaterThanOrEqual(taxPercentageFor24Months)
+    expect(taxPercentageFor6Months).toBeGreaterThan(taxPercentageFor12Months)
+    expect(taxPercentageFor12Months).toBeGreaterThan(taxPercentageFor18Months)
+    expect(taxPercentageFor18Months).toBeGreaterThan(taxPercentageFor24Months)
   })
 })
