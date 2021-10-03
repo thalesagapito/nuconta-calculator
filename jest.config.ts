be112f5423eed46b85634ac9fdd2c6bd34eb1616
Thales Agapito <thsousa101@gmail.com>
2021-10-03 12:44:12 -0300

    add jest config collectCoverageFrom option

--- jest.config.ts
+++ jest.config.ts
@@ -21,7 +21,10 @@ export default {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: [
+    'src/domain/**/*.{ts,vue}',
+    '!**/*.d.ts',
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
