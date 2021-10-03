775fe21f464e4903347289ba11d4bd135fe49ed8
Thales Agapito <thsousa101@gmail.com>
2021-10-03 12:54:39 -0300

    add nu-conta constants file and exclude it from jest coverage

---
+++ src/domain/nu-conta/constants.ts
@@ -0,0 +1,8 @@
+import { Big } from 'big.js'
+
+export const DECIMAL_PLACES_FOR_RESULT = 2
+export const DECIMAL_PLACES_FOR_CALCULATIONS = 5
+
+Big.DP = DECIMAL_PLACES_FOR_CALCULATIONS
+
+export const BigJs = Big
